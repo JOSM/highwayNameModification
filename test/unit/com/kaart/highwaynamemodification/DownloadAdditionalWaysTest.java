fbc56b9ecf58d17d0050270dc29ee3bef6fc573a
Taylor Smock <taylor.smock@kaart.com>
2019-12-04 16:13:34 -0700

    Add failing test to build directory path along with updating build files
    Signed-off-by: Taylor Smock <taylor.smock@kaart.com>

---
+++ test/unit/com/kaart/highwaynamemodification/DownloadAdditionalWaysTest.java
@@ -0,0 +1,19 @@
+package com.kaart.highwaynamemodification;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DownloadAdditionalWaysTest {
+
+    @Test
+    public void testCheckIfDownloaded() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetAdditionalWays() {
+        fail("Not yet implemented");
+    }
+
+}
