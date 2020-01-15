74f4082e548d0c333fc28a27313acdfca9b10f93
Rytheking <ryanelliottfleming@gmail.com>
2020-01-15 16:41:36 -0700

    Tests added

---
+++ test/unit/com/kaart/highwaynamemodification/HighwayNameListenerTest.java
@@ -0,0 +1,77 @@
+package com.kaart.highwaynamemodification;
+
+import static org.junit.Assert.*;
+
+import javax.swing.JOptionPane;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.Before;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+import org.openstreetmap.josm.data.osm.event.DataChangedEvent;
+import org.openstreetmap.josm.io.OsmApi;
+import org.openstreetmap.josm.spi.preferences.Config;
+import org.openstreetmap.josm.TestUtils;
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Node;
+
+/**
+ * @author Ryan Fleming
+ *
+ */
+public class HighwayNameListenerTest {
+	 @Rule
+	    public JOSMTestRules rule = new JOSMTestRules().projection();
+
+	    WireMockServer wireMock = new WireMockServer(options().usingFilesUnderDirectory("test/resources/wiremock"));
+
+	 @Before
+	 	public void setUp() {
+	        wireMock.start();
+	        Config.getPref().put("osm-server.url", wireMock.baseUrl()); // TODO do overpass, not OSM server...
+			Config.getPref().putBoolean("message." + HighwayNameModification.NAME +".downloadAdditional", false);
+			Config.getPref().putInt("message." + HighwayNameModification.NAME + ".downloadAdditional" + ".value", JOptionPane.YES_OPTION);
+	        }
+
+	 @After
+	    public void tearDown() {
+	        wireMock.stop();
+	        Config.getPref().put("osm-server.url", Config.getUrls().getDefaultOsmApiUrl());
+	    }
+
+	 @Test
+	 	public final void testTagsChanged() {
+			HighwayNameListener tester = new HighwayNameListener();
+			Way prim = TestUtils.newWay("highway=residential name=\"North 8th Street\"", new Node(new LatLon(39.084616, -108.559293)), new Node(new LatLon(39.0854611, -108.5592888)));
+			DataSet newDataset = new DataSet();
+			prim.getNodes().forEach(newDataset::addPrimitive);
+			newDataset.addPrimitive(prim);
+			newDataset.addDataSetListener(tester);
+			wireMock.startRecording(Config.getUrls().getDefaultOsmApiUrl()); // TODO save mappings and remove
+			prim.put("name", "Road 2");
+			wireMock.stopRecording();
+			wireMock.saveMappings();
+			assertTrue(newDataset.getWays().stream().filter(way ->way.hasTag("addr:street")).allMatch(way -> "Road 2".equals(way.get("addr:street"))));
+			
+		}
+	
+	@Test
+		public final void testDataChanged() {
+			HighwayNameListener tester = new HighwayNameListener();
+			OsmPrimitive prim = TestUtils.newWay("highway=residential name=\"Road 1\"", new Node(new LatLon(0, 0)), new Node(new LatLon(1, 1)));
+			prim.put("name", "Road 2");
+			DataChangedEvent event = new DataChangedEvent(new DataSet());
+			tester.dataChanged(event);
+			assertTrue(ModifyWays.getInstance().downloadTask);
+			
+		}
+}
