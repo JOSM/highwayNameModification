266b8a71d655382f59de0165e4f1ea16a933ed2d
Rytheking <ryanelliottfleming@gmail.com>
2020-01-27 19:49:27 -0700

    Made plugin restartless & add ChangeActionTests

--- src/main/java/com/kaart/highwaynamemodification/HighwayNameModification.java
+++ src/main/java/com/kaart/highwaynamemodification/HighwayNameModification.java
@@ -1,28 +1,56 @@
 package com.kaart.highwaynamemodification;
 
-import javax.swing.AbstractAction;
+import java.awt.Component;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import org.openstreetmap.josm.data.osm.event.DatasetEventManager;
+import org.openstreetmap.josm.data.osm.event.DatasetEventManager.FireMode;
 import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
 import org.openstreetmap.josm.tools.ImageProvider;
+import org.openstreetmap.josm.tools.Destroyable;
 
 /**
  *
  * @author Taylor Smock
  *
  */
-public class HighwayNameModification extends Plugin {
+public class HighwayNameModification extends Plugin implements Destroyable{
 	public static final String NAME = "Highway Name Modification";
 	public static final String PLUGIN_IMAGE = "deltasignmod";
+	private HighwayNameListener listener;
+	private AbstractAction highwayNameModificationAction;
 
 	public HighwayNameModification(PluginInformation info) {
 		super(info);
-		HighwayNameModificationLayerChangeListener listener = new HighwayNameModificationLayerChangeListener();
-		MainApplication.getLayerManager().addLayerChangeListener(listener);
-
-		AbstractAction highwayNameModificationAction = new HighwayNameChangeAction(NAME,
+		listener = new HighwayNameListener();
+		
+		DatasetEventManager.getInstance().addDatasetListener(listener, FireMode.IMMEDIATELY);
+		highwayNameModificationAction = new HighwayNameChangeAction(NAME,
 				ImageProvider.get(PLUGIN_IMAGE, ImageProvider.ImageSizes.MENU), listener);
 		MainApplication.getMenu().dataMenu.add(highwayNameModificationAction);
+	}
+	@Override
+	public void destroy() {
+		final JMenu dataMenu = MainApplication.getMenu().dataMenu;
+		DatasetEventManager.getInstance().removeDatasetListener(listener);
+		final Map<Action, Component> actions = Arrays.asList(dataMenu.getMenuComponents()).stream()
+                .filter(JMenuItem.class::isInstance).map(JMenuItem.class::cast)
+                .collect(Collectors.toMap(JMenuItem::getAction, component -> component));
+
+        for (final Entry<Action, Component> action : actions.entrySet()) {
+            if (highwayNameModificationAction.equals(action.getKey())) {
+                dataMenu.remove(action.getValue());
+            }
+        }
 	}
 }
---
+++ test/unit/com/kaart/highwaynamemodification/HighwayNameChangeActionTest.java
@@ -0,0 +1,66 @@
+package com.kaart.highwaynamemodification;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.junit.Assert.*;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JOptionPane;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openstreetmap.josm.TestUtils;
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.data.osm.DataSet;
+import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.spi.preferences.Config;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+public class HighwayNameChangeActionTest {
+	@Rule
+    public JOSMTestRules rule = new JOSMTestRules().projection();
+    WireMockServer wireMock = new WireMockServer(options().usingFilesUnderDirectory("test/resources/wiremock"));
+
+ @Before
+ 	public void setUp()    {
+        wireMock.start();
+        
+        Config.getPref().put("osm-server.url", wireMock.baseUrl()); // TODO do overpass, not OSM server...
+		Config.getPref().putBoolean("message." + HighwayNameModification.NAME +".downloadAdditional", false);
+		Config.getPref().putInt("message." + HighwayNameModification.NAME + ".downloadAdditional" + ".value", JOptionPane.YES_OPTION);
+		
+    }
+
+ @After
+    public void tearDown() {
+        wireMock.stop();
+        Config.getPref().put("osm-server.url", Config.getUrls().getDefaultOsmApiUrl());
+        
+    }
+
+	@Test
+	public final void testActionPerformed() {
+		HighwayNameListener tester = new HighwayNameListener();
+		Way prim = TestUtils.newWay("highway=residential name=\"North 8th Street\"", new Node(new LatLon(39.084616, -108.559293)), new Node(new LatLon(39.0854611, -108.5592888)));
+		DataSet newDataset = new DataSet();
+		prim.getNodes().forEach(newDataset::addPrimitive);
+		newDataset.addPrimitive(prim);
+		newDataset.addDataSetListener(tester);
+		wireMock.startRecording(Config.getUrls().getDefaultOsmApiUrl()); // TODO save mappings and remove
+		prim.put("name", "Road 2");
+		wireMock.stopRecording();
+		wireMock.saveMappings();
+		assertTrue(newDataset.getWays().stream().filter(way -> way.hasTag("name")).allMatch(way -> "Road 2".equals(way.get("name"))));
+		wireMock.startRecording(Config.getUrls().getDefaultOsmApiUrl()); // TODO save mappings and remove
+		prim.put("highway", "residential");
+		wireMock.stopRecording();
+		wireMock.saveMappings();
+		assertTrue(newDataset.getWays().stream().filter(way -> way.hasTag("name")).allMatch(way -> "Road 2".equals(way.get("name"))));
+	}
+
+}
