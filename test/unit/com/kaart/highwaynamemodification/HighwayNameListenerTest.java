914845c4ce30a8cf6ceeb1a9b782548d2c835985
Taylor Smock <taylor.smock@kaart.com>
2020-02-04 11:07:47 -0700

    Initial WireMock support (reduce network requests by 30 towards overpass)
    Signed-off-by: Taylor Smock <taylor.smock@kaart.com>

--- test/unit/com/kaart/highwaynamemodification/HighwayNameListenerTest.java
+++ test/unit/com/kaart/highwaynamemodification/HighwayNameListenerTest.java
@@ -1,25 +1,23 @@
 package com.kaart.highwaynamemodification;
 
-import static org.junit.Assert.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.junit.Assert.assertTrue;
 
 import javax.swing.JOptionPane;
 
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.Before;
-
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-import org.openstreetmap.josm.data.osm.Way;
-import org.openstreetmap.josm.testutils.JOSMTestRules;
-
-import com.github.tomakehurst.wiremock.WireMockServer;
-
-import org.openstreetmap.josm.spi.preferences.Config;
 import org.openstreetmap.josm.TestUtils;
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.osm.DataSet;
 import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Way;
+import org.openstreetmap.josm.spi.preferences.Config;
+import org.openstreetmap.josm.testutils.JOSMTestRules;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
 
 /**
  * @author Ryan Fleming
@@ -27,14 +25,14 @@ import org.openstreetmap.josm.data.osm.Node;
  */
 public class HighwayNameListenerTest {
     @Rule
-    public JOSMTestRules rule = new JOSMTestRules().projection();
+    public JOSMTestRules rule = new JOSMTestRules().projection().preferences().fakeAPI();
     WireMockServer wireMock = new WireMockServer(options().usingFilesUnderDirectory("test/resources/wiremock"));
 
     @Before
     public void setUp() {
         wireMock.start();
 
-        Config.getPref().put("download.overpass.server", wireMock.baseUrl()); // TODO do overpass, not OSM server...
+        Config.getPref().put("download.overpass.server", wireMock.baseUrl());
         Config.getPref().putBoolean("message." + HighwayNameModification.NAME + ".downloadAdditional", false);
         Config.getPref().putInt("message." + HighwayNameModification.NAME + ".downloadAdditional" + ".value",
                 JOptionPane.YES_OPTION);
@@ -45,6 +43,7 @@ public class HighwayNameListenerTest {
     public void tearDown() {
         wireMock.stop();
         Config.getPref().put("osm-server.url", Config.getUrls().getDefaultOsmApiUrl());
+        Config.getPref().put("download.overpass.server", "https://overpass-api.de/api/");
 
     }
 
@@ -57,10 +56,7 @@ public class HighwayNameListenerTest {
         prim.getNodes().forEach(newDataset::addPrimitive);
         newDataset.addPrimitive(prim);
         newDataset.addDataSetListener(tester);
-        wireMock.startRecording(Config.getUrls().getDefaultOsmApiUrl()); // TODO save mappings and remove
         prim.put("name", "Road 2");
-        wireMock.stopRecording();
-        wireMock.saveMappings();
         assertTrue(newDataset.getWays().stream().filter(way -> way.hasTag("addr:street"))
                 .allMatch(way -> "Road 2".equals(way.get("addr:street"))));
 
@@ -75,7 +71,6 @@ public class HighwayNameListenerTest {
         prim.getNodes().forEach(newDataset::addPrimitive);
         newDataset.addPrimitive(prim);
         newDataset.addDataSetListener(tester);
-        wireMock.startRecording(Config.getUrls().getDefaultOsmApiUrl()); // TODO save mappings and remove
         for (int i = 0; i < 31; i++) { // 30 is minimum single stack events to trigger a DataChangedEvent
             Way newprim = TestUtils.newWay("highway=residential name=\"North 8th Street\"",
                     new Node(new LatLon(39.084616, -108.559293 + (i / 100))),
@@ -85,8 +80,6 @@ public class HighwayNameListenerTest {
             newprim.put("name", "Road " + i);
 
         }
-        wireMock.stopRecording();
-        wireMock.saveMappings();
         assertTrue(newDataset.getWays().stream().filter(way -> way.hasTag("addr:street"))
                 .allMatch(way -> "Road 2".equals(way.get("addr:street"))));
 
